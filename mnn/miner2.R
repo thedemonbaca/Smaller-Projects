a3844692e113643d715c378f3d5ef4186a571608
Bonza-Times <tylers.pile@gmail.com>
2013-09-26 20:55:53 -0400

    count changes

--- mnn/miner2.R
+++ mnn/miner2.R
@@ -48,6 +48,9 @@ Northeast.Pioneer.Valley <- sqldf("SELECT * FROM data2 WHERE Region =
 # Southeast
 Southeast <-  sqldf("SELECT * FROM data2 WHERE Region = 'Southeast'
 ORDER BY Sector")
+# Berkshire
+Berkshire <-  sqldf("SELECT * FROM data2 WHERE Region = 'Berkshire'
+ORDER BY Sector")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Write regions out to a csv                                      # 
@@ -55,14 +58,51 @@ ORDER BY Sector")
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Boston
-write.csv(MyData, file = "MyData.csv")
+write.csv(Boston, file = "Boston.csv")
 # Cape & Islands
-write.csv(MyData, file = "MyData.csv")
+write.csv(Cape.Island, file = "CapeIsland.csv")
 # Central
-write.csv(MyData, file = "MyData.csv")
+write.csv(Central, file = "Central.csv")
 # Metrowest
-write.csv(MyData, file = "MyData.csv")
+write.csv(Metrowest, file = "Metrowest.csv")
 # Northeast Pioneer Valley
 write.csv(Northeast.Pioneer.Valley, file = "NortheastPioneerValley.csv")
 # Southeast
 write.csv(Southeast, file = "Southeast.csv")
+# Berkshire
+write.csv(Berkshire, file = "Berkshire.csv")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Count number of MNN Members                                     # 
+#                                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Boston
+Boston.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Boston' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+Boston.A
+# Cape & Islands
+Central.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Central' AND MNN_Name != 'NA' GROUP BY MNN_Name") # Check here
+Central.A
+# Central
+Central.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Central' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+Central.A
+# Metrowest
+Metrowest.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Metrowest' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+Metrowest.A
+# Northeast Pioneer Valley
+NortheastPioneerValley.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Northeast Pioneer Valley' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+NortheastPioneerValley.A
+# Southeast
+Southeast.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Southeast' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+Southeast.A
+# Berkshire
+Berkshire.A <-  sqldf("SELECT MNN_Name, COUNT(MNN_Name) FROM data2 WHERE
+Region = 'Berkshire' AND MNN_Name != 'NA' GROUP BY MNN_Name")
+Berkshire.A
+
