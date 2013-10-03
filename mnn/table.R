6e6b1311c6f52cec8387080fdbd897e3288baa16
Bonza-Times <tylers.pile@gmail.com>
2013-10-03 18:14:15 -0400

    draft for review

--- mnn/table.R
+++ mnn/table.R
@@ -14,9 +14,24 @@ budget <-c("Under 25k", "25k-75k", "75k-250k", "250k-750k", "750k-2.5m", "2.5m-1
 bl <- factor(mnn.dat$NCCS_2011_Budget.Range, levels=budget.levels)
 mytable <- table(mnn.dat$Region, bl)
 
-margin.table(mytable, 1) # mnn.dat$Region frequencies (summed over bl)
-margin.table(mytable, 2) # bl frequencies (summed over mnn.dat$Region)
-
+## Picking up from reference
 prop.table(mytable) # cell percentages
 prop.table(mytable, 1) # row percentages
 prop.table(mytable, 2) # column percentages 
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Here's what I have in mind
+
+
+# Figure 1: Comparison of Region, Budget Range, and MNN Status
+# 3-Way Frequency Table
+mytable2 <- table(mnn.dat$Region, bl, mnn.dat$MNN.Status)
+ftable(mytable2)
+
+# Figure 2: Sum of table entries for a given index
+margin.table(mytable2, 1) # mnn.dat$Region frequencies (summed over bl)
+margin.table(mytable2, 2) # bl frequencies (summed over mnn.dat$Region)
+margin.table(mytable2, 3) # mnn.dat$MNN.Status (summed over mnn.dat$Region)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
